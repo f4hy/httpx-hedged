5998cae8ebcb12e509fa9768237d906c362d8fab
Brendan Fahy <bmfahy@gmail.com>
2025-11-24 02:33:28 -0500

    rename

--- test_httpx_hedged.py
+++ test_httpx_hedged.py
@@ -11,7 +11,7 @@ from unittest.mock import AsyncMock, Mock
 import httpx
 import pytest
 
-from httpx_hedging import (
+from httpx_hedged import (
     LatencyTracker,
     PercentileHedgingClient,
     PercentileHedgingTransport,
